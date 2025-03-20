2f8c5bdd7b72a5adf2916a14a205dd8755895ca6
rtNelson3804 <randall.nelson28@gmail.com>
2025-03-19 19:02:28 -0500

    Randall

--- histo_plot.js
+++ histo_plot.js
@@ -1,10 +1,14 @@
 import neoData from './neo_data.js';
 
 // Extract estimated diameters
-const diameters = neoData.map(neo => (neo.estimated_diameter_km_min + neo.estimated_diameter_km_max) / 2);
+const diameters = neoData
+  .map(neo => ((neo.estimated_diameter_km_min + neo.estimated_diameter_km_max) / 2) * 1000)
+  .filter(diameter => diameter < 500);
 
 // Filter for diameters greater than 0.5 km
-const diametersOverHalfKm = diameters.filter(diameter => diameter > 0.5);
+const diametersOverHalfKm = neoData
+    .map(neo => (neo.estimated_diameter_km_min + neo.estimated_diameter_km_max) / 2)
+    .filter(diameter => diameter > 0.5 && diameter < 5);
 
 // Create histogram data for all diameters
 const traceAllDiameters = {
@@ -24,8 +28,21 @@ const traceOverHalfKm = {
     name: 'Diameters > 0.5 km' // Label for the second histogram
 };
 
-const layout = {
-    title: 'Distribution of Asteroid Diameters',
+const layout1 = {
+    title: 'Distribution of Asteroid Diameters Smaller Than 0.5 km',
+    xaxis: {
+        title: 'Diameter (Meters)',
+        showgrid: true
+    },
+    yaxis: {
+        title: 'Count',
+        showgrid: true
+    },
+    bargap: 0.1
+};
+
+const layout2 = {
+    title: 'Distribution of Asteroid Diameters Larger Than 0.5 km',
     xaxis: {
         title: 'Diameter (km)',
         showgrid: true
@@ -38,5 +55,5 @@ const layout = {
 };
 
 // Render the histograms together in the same plot
-Plotly.newPlot('histogram', [traceAllDiameters], layout);
-Plotly.newPlot('histogram2', [traceOverHalfKm], layout);
+Plotly.newPlot('histogram', [traceAllDiameters], layout1);
+Plotly.newPlot('histogram2', [traceOverHalfKm], layout2);

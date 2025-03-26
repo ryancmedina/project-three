4808a10c20ed1264f145148ea990f99689d996ba
ryancmedina <ryancmedina42@gmail.com>
2025-03-25 20:21:07 -0500

    3/25

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,4 @@
-let graphData = [];
-let nS = "";
+let neoerData = neoData;
 
 function init()
 { 
@@ -12,31 +11,30 @@ function init()
   dropdown2.append("option").text("Two Weeks")
   dropdown2.append("option").text("Three Weeks")
   dropdown2.append("option").text("Four Weeks")
-  //dropdown2.append("option").text("Five Weeks")
 
   checkbox = d3.select("#myCheckbox");
   checkbox.property("checked", true);
- 
-  numberChanged("One Week");
   
+  neoerData.sort((a, b) => {
+    return new Date(a.close_approach_date) - new Date(b.close_approach_date);
+  })
+
+  numberChanged("One Week");
 }
 
 init();
 
 function data_managment(h)
 {
-  console.log(neoData[0]);
-  //console.log(neoData[0].close_approach_date.charAt(6));
-  //console.log(neoData)
-  //console.log(dropdown2.property("value"))
-  console.log(h);
-  let x_marks = [];
-  let y_marks = [];
-
-  x_marks = h.map(item => item.relative_velocity_kph);
-  y_marks = h.map(item => item.estimated_diameter_km_max);
+  let x_marks = h.map(item => item.relative_velocity_kph);
+  let y_marks = h.map(item => item.estimated_diameter_km_max);
   names = h.map(item => item.name);
 
+  for(i = 0; i < names.length; i++)
+  {
+    names[i] = "Name: " + names[i];
+  }
+  
   let trace  ={
     x: x_marks,
     y: y_marks,
@@ -45,7 +43,7 @@ function data_managment(h)
     mode: "markers",
     marker: {
       size: 10,
-      color: "red",
+      color: "black",
       hovertemplate: "h"
     },
     hoverinfo: "text"
@@ -56,58 +54,51 @@ function data_managment(h)
   let layout = {
     title: "Velocity vs Diameter of Asteroids Near Earth",
     xaxis: {
-      title : "Relative Velocity (Kph)"
+      title : "Relative Velocity (Kph)",
+      range: [0, 130000]
     },
     yaxis: {
-      title: "Estimated Max Diameter (KM)"
+      title: "Estimated Max Diameter (KM)",
+      range: [0, 1.5]
     },
+    height: 800,
     hovermode : "closest"
   }
 
-  Plotly.newPlot("bubble", scatter, layout);
+  Plotly.newPlot("plot", scatter, layout);
 }
 
   function numberChanged(newSample) {
-    //buildCharts(newSample);
-    //buildMetadata(newSample);
-
     let month = getWeeks(newSample); 
     graphData = []
     let weekCheck = month * 7;
-    let day = parseInt(neoData[0].close_approach_date.charAt(8) + neoData[0].close_approach_date.charAt(9))
+    let day = parseInt(neoerData[0].close_approach_date.charAt(8) + neoerData[0].close_approach_date.charAt(9))
     let d = 0;
     let w = 0;
 
     for(let i = 0; i < neoData.length; i++)
     {
-      d = parseInt(neoData[i].close_approach_date.charAt(8) + neoData[i].close_approach_date.charAt(9));
+      d = parseInt(neoerData[i].close_approach_date.charAt(8) + neoerData[i].close_approach_date.charAt(9));
 
       if(d != day)
       { 
         w += 1;
         day = d;
-        console.log(`d = ${d}, day = ${day}`)
         if(w >= weekCheck)
           break;
       }
      
-      //neoData[i].close_approach_date.charAt(6) === month) && 
-      if((checkbox.property("checked") || neoData[i].is_potentially_hazardous))
+      if((checkbox.property("checked") || neoerData[i].is_potentially_hazardous))
       {
         graphData.push(neoData[i]);
       }
-
-      
     }
-
-
 
     data_managment(graphData)
   }
 
   function sortData()
-  {
-    console.log("change")  
+  { 
     numberChanged(dropdown2.property("value"));
   }
 
